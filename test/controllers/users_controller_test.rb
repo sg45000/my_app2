db9e468b1ed9c9f19dc711659131b1a5470369c0
EC2 Default User <ec2-user@ip-172-31-21-102.us-east-2.compute.internal>
2019-01-27 09:40:56 +0000

    Users model create

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get users_new_url
-    assert_response :success
-  end
+  
+  # def setup
+  #   @user = users(:micheal)
+  # end
+  
+  # test "should get new" do
+  #   get new_user_url
+  #   assert_response :success
+  # end
+  
+  
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,49 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   def setup
+     @user = User.new(name: "sample", email: "sample@sample.com",
+              password: "foobar", password_confirmation: "foobar")
+   end
+   
+   test "should be valid" do
+     assert @user.valid?
+   end
+   
+   test "name wrong length should be invalid" do
+     @user.name = "abc"
+     assert_not @user.valid?
+     
+     @user.name = "a"*21
+     assert_not @user.valid?
+   end
+   
+   test "blank name should be invalid" do
+     @user.name = "   "
+     assert_not @user.valid?
+   end
+   
+   test "blank email should be invalid" do
+     @user.email = "   "
+     assert_not @user.valid?
+   end
+   
+   test "wrong mail address should be invalid" do
+     @user.email = "abc@com"
+     assert_not @user.valid?
+   end
+   
+   test "wrong password should be invalid" do
+     @user.password = "foo"
+     @user.password_confirmation = "bar"
+     assert_not @user.valid?
+   end
+   
+   test "email uniqueness test" do
+     @user.save
+     assert_no_difference "User.count" do
+       @user.save
+     end
+   end
+   
 end
