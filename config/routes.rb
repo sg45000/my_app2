74a14d6a21f277bf40bf505c653b3123097fe8bd
EC2 Default User <ec2-user@ip-172-31-21-102.us-east-2.compute.internal>
2019-02-10 01:55:44 +0000

    Add basic login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'static_page#home'
   get '/about' => 'static_page#about'
   get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class StaticPageControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_page_home_url
+    get root_url
     assert_response :success
   end
 
   test "should get about" do
-    get static_page_about_url
+    get about_url
     assert_response :success
   end
 
