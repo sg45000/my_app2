1af4b8b9beab9b727eb1ec719e703c23b69ce2c1
EC2 Default User <ec2-user@ip-172-31-21-102.us-east-2.compute.internal>
2019-02-24 01:47:10 +0000

    add basic_genre_tag

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+CSV.foreach('db/genre.csv')do |row|
+    Genre.create(name: row[0])
+end
